701838b50d3614e7d4e3eefcf72984d7787ef952
Mickle Isaev <mrraptor26@gmail.com>
2019-11-01 10:53:18 +0300

    refature (IMMPC) доабвлены функции расчета CRC

--- Lib_A_IIMPC_imm_protocol_communicate.c
+++ Lib_A_IIMPC_imm_protocol_communicate.c
@@ -1,7 +1,7 @@
-/** 
+/**
  * @file   	%<%NAME%>%.%<%EXTENSION%>%
  * @author 	%<%USER%>%
- * @version	
+ * @version
  * @date 	%<%DATE%>%, %<%TIME%>%
  * @brief
  */
@@ -21,10 +21,129 @@
 
 
 /*#### |Begin| --> Секция - "Прототипы локальных функций" ####################*/
+static uint16_t
+IMMPC_GetCRC_Generic(
+	uint8_t *pData,
+	uint16_t len);
+
+static uint16_t
+IMMPC_GetCRC_9dof_main_raw_pack(
+	immpc_9dof_main_raw_pack_s *pPack_s);
+
+static size_t
+IMMPC_IsPackValid_9dof_main_raw_pack(
+	immpc_9dof_main_raw_pack_s *pPack_s);
 /*#### |End  | <-- Секция - "Прототипы локальных функций" ####################*/
 
 
 /*#### |Begin| --> Секция - "Описание глобальных функций" ####################*/
+void
+IMMPC_GetTypeMessage(
+	uint8_t *pData,
+	size_t 	buffSize,
+	uint8_t *pMessageIDReturn,
+	uint8_t *pMessagePackReturn)
+{
+	/* Найти в потоке данных символы StartFrame */
+
+	/* Посмотреть какой ID сообщения */
+
+	/* Записать в pMessagePackReturn номер, который соответствует структуре данных */
+}
+
+/*-------------------------------------------------------------------------*//**
+ * @author    Mickle Isaev
+ * @date      01-ноя-2019
+ *
+ * @brief    Функция вычисляет контрольную сумму
+ *
+ * @param[in] 	*pData: 	Указатель на область памяти, в которой содержится 
+ * 							пакет данных
+ * @param[in]   len:	Количество байт пакета данных
+ *
+ * @return    { description_of_the_return_value }
+ */
+static uint16_t
+IMMPC_GetCRC_Generic(
+	uint8_t *pData,
+	uint16_t len)
+{
+	return (CRC_XOR_CCITT_Poly0x1021_Crc16(
+				pData[2u],
+				len - 4u));
+}
+
+/* Пакет 9dof_main_raw_pack_s -->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> */
+
+/*-------------------------------------------------------------------------*//**
+ * @author    Mickle Isaev
+ * @date      01-ноя-2019
+ *
+ * @brief    Функция выполняет вычисление контрольной суммы пакета данных 
+ *
+ * @param[in]	*pPack_s: 	Указатель на область памяти, в которой содержится 
+ * 							пакет данных 
+ *
+ * @return 	Вычисленная сумма пакета данных
+ */
+static uint16_t
+IMMPC_GetCRC_9dof_main_raw_pack(
+	immpc_9dof_main_raw_pack_s *pPack_s)
+{
+	retrun (IMMPC_GetCRC_Generic(
+				(uint8_t*) pPack_s,
+				(uint16_t) (sizeof(immpc_9dof_main_raw_pack_s))));
+}
+
+/*-------------------------------------------------------------------------*//**
+ * @author    Mickle Isaev
+ * @date      01-ноя-2019
+ *
+ * @brief 	Функция выполняет запись контрольной суммы пакета данных 
+ *
+ * @param[in,out]	*pPack_s: 	Указатель на область памяти, в которой содержится 
+ * 								пакет данных.
+ * 								@note 	Контрольная сумма записывается в конец 
+ * 										пакета данных
+ */
+static void
+IMMPC_WriteCRC_9dof_main_raw_pack(
+	immpc_9dof_main_raw_pack_s *pPack_s)
+{
+	pPack_s->crc =
+		IMMPC_GetCRC_Generic(
+			(uint8_t*) pPack_s,
+			(uint16_t) (sizeof(immpc_9dof_main_raw_pack_s)));
+}
+
+/*-------------------------------------------------------------------------*//**
+ * @author    Mickle Isaev
+ * @date      01-ноя-2019
+ *
+ * @brief Функция проверяет валидность пакета данных 
+ *
+ * @param[in]	*pPack_s: 	Указатель на область памяти, в которой содержится 
+ * 							пакет данных.
+ *
+ * @return 	Если пакет данных валиден, то возвращает "1", иначе "0"
+ */
+static size_t
+IMMPC_IsPackValid_9dof_main_raw_pack(
+	immpc_9dof_main_raw_pack_s *pPack_s)
+{
+	uint16_t crc =
+		IMMPC_GetCRC_9dof_main_raw_pack_s(pPack_s);
+
+	if (crc == pPack_s->crc)
+	{
+		return (1u);
+	}
+	else
+	{
+		return (0u);
+	}
+}
+/* Пакет 9dof_main_raw_pack_s --<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< */
 /*#### |End  | <-- Секция - "Описание глобальных функций" ####################*/
 
 
--- Lib_A_IIMPC_imm_protocol_communicate.h
+++ Lib_A_IIMPC_imm_protocol_communicate.h
@@ -1,8 +1,8 @@
-/** 
- * @file   	%<%NAME%>%.%<%EXTENSION%>%
- * @author 	%<%USER%>%
- * @version	
- * @date 	%<%DATE%>%, %<%TIME%>%
+/**
+ * @file    %<%NAME%>%.%<%EXTENSION%>%
+ * @author  %<%USER%>%
+ * @version
+ * @date    %<%DATE%>%, %<%TIME%>%
  * @brief
  */
 
@@ -13,6 +13,7 @@
 
 /*#### |Begin| --> Секция - "Include" ########################################*/
 /*==== |Begin| --> Секция - "C libraries" ====================================*/
+#include "stdint.h"
 /*==== |End  | <-- Секция - "C libraries" ====================================*/
 
 /*==== |Begin| --> Секция - "RTOS libraries ==================================*/
@@ -22,11 +23,178 @@
 /*==== |End  | <-- Секция - "MK peripheral libraries" ========================*/
 
 /*==== |Begin| --> Секция - "Extern libraries" ===============================*/
+#include "Lib_A_CRC_cyclic_redundancy_check.h"
 /*==== |End  | <-- Секция - "Extern libraries" ===============================*/
 /*#### |End  | <-- Секция - "Include" ########################################*/
 
 
 /*#### |Begin| --> Секция - "Определение констант" ###########################*/
+
+/*==== |Begin| --> Секция определения типа числа с плавающей точкой ==========*/
+#if !defined (__IMMPC_FPT__)
+	#error "Please, set __IMMPC_FPT__ float or double in macros list"
+#endif
+
+#if !defined (__IMMPC_FPT_SIZE__)
+	#error "Please, set __IMMPC_FPT_SIZE__ 4 (that mean float) or 8 (that mean double) in macros list"
+#endif
+
+#if     __IMMPC_FPT_SIZE__ == 4
+
+#elif   __IMMPC_FPT_SIZE__ == 8
+
+#else
+	#error "Your compiler uses a non-standard floating point size"
+#endif
+/*==== |End  | <-- Секция определения типа числа с плавающей точкой ==========*/
+
+/*==== |Begin| --> Секция - Макросы для встраиваемых функций =================*/
+#if defined (__GNUC__)
+
+	/* inline*/
+	#ifndef __IMMPC_INLINE
+		#define __IMMPC_INLINE             inline
+	#endif
+
+	/* static inline */
+	#ifndef __IMMPC_STATIC_INLINE
+		#define __IMMPC_STATIC_INLINE      static inline
+	#endif
+
+	/* always inline */
+	#ifndef __IMMPC_ALWAYS_INLINE
+		#define __IMMPC_ALWAYS_INLINE      inline __attribute__((always_inline)) static
+	#endif
+
+	/* force inline */
+	#ifndef __IMMPC_FORCE_INLINE
+		#define __IMMPC_FORCE_INLINE       inline __attribute__((always_inline))
+	#endif
+
+#else
+	#define __IMMPC_INLINE
+	#define __IMMPC_STATIC_INLINE   static
+	#define __IMMPC_ALWAYS_INLINE
+#endif
+/*==== |End  | <-- Секция - Макросы для встраиваемых функций =================*/
+
+/*==== |Begin| --> Секция - Расположение функций библиотеки в специальной
+ *                          области памяти ===================================*/
+#if defined (__IMMPC_FNC_ONCE_MEMORY_LOCATION_NAME__)
+	#if defined (__GNUC__)
+		#define __IMMPC_FNC_ONCE_MEMORY_LOCATION  __attribute__ ((section(__IMMPC_FNC_ONCE_MEMORY_LOCATION_NAME__)))
+	#else
+		#error "You defined the name of the memory area for the function location, but the type of your compiler is not supported by the library. You can delete the macro definition __IMMPC_FNC_ONCE_MEMORY_LOCATION_NAME__ or extend the macro definition __IMMPC_FNC_ONCE_MEMORY_LOCATION for your compiler type"
+	#endif
+#else
+	#define __IMMPC_FNC_ONCE_MEMORY_LOCATION
+#endif
+
+#if defined (__IMMPC_FNC_LOOP_MEMORY_LOCATION_NAME__)
+	#if defined (__GNUC__)
+		#define __IMMPC_FNC_LOOP_MEMORY_LOCATION  __attribute__ ((section(__IMMPC_FNC_LOOP_MEMORY_LOCATION_NAME__)))
+	#else
+		#error "You defined the name of the memory area for the function location, but the type of your compiler is not supported by the library. You can delete the macro definition __IMMPC_FNC_LOOP_MEMORY_LOCATION_NAME__ or extend the macro definition __IMMPC_FNC_LOOP_MEMORY_LOCATION for your compiler type"
+	#endif
+#else
+	#define __IMMPC_FNC_LOOP_MEMORY_LOCATION
+#endif
+/*==== |End  | <-- Секция - Расположение функций библиотеки в специальной
+ *                          области памяти ===================================*/
+
+
+/*==== |Begin| --> Секция - Расположение констант библиотеки в специальной
+ *                          области памяти ===================================*/
+#if defined (__IMMPC_CONST_MEMORY_LOCATION_NAME__)
+	#if defined (__GNUC__)
+		#define __IMMPC_CONST_MEMORY_LOCATION  __attribute__ ((section(__IMMPC_CONST_MEMORY_LOCATION_NAME__)))
+	#else
+		#error "You defined the name of the memory area for the function location, but the type of your compiler is not supported by the library. You can delete the macro definition __IMMPC_CONST_LOOP_MEMORY_LOCATION_NAME__ or extend the macro definition __IMMPC_CONST_MEMORY_LOCATION for your compiler type"
+	#endif
+#else
+	#define __IMMPC_CONST_MEMORY_LOCATION
+#endif
+/*==== |End  | <-- Секция - Расположение констант библиотеки в специальной
+ *                          области памяти ===================================*/
+
+/*==== |Begin| --> Секция - Локальная оптимизация функций ====================*/
+#if defined (__GNUC__)
+	#ifndef __IMMPC_FNC_ONCE_OPTIMIZE_MODE
+		#define __IMMPC_FNC_ONCE_OPTIMIZE_MODE
+	#endif
+#endif
+
+#if defined (__GNUC__)
+	#ifndef __IMMPC_FNC_LOOP_OPTIMIZE_MODE
+		#define __IMMPC_FNC_LOOP_OPTIMIZE_MODE
+	#endif
+#endif
+/*==== |End| --> Секция - Локальная оптимизация функций ======================*/
+
+#define IIMPC_START_FRAME								((uint16_t)0xAAAA)
+
+#define IIMPC_PACK_REQUESTS_BITS_CALIB_MEAS             0b10000000
+#define IIMPC_PACK_REQUESTS_BITS_RESERV_MEAS            0b01000000
+#define IIMPC_PACK_REQUESTS_BITS_READ_MEAS              0b00100000
+#define IIMPC_PACK_REQUESTS_BITS_IS_DATA_REQUEST        0b00010000
+
+#define IIMPC_PACK_REQUESTS_BITS_SET_CALIB_MEAS(var)    (var | IIMPC_PACK_REQUESTS_BITS_CALIB_MEAS)
+#define IIMPC_PACK_REQUESTS_BITS_SET_RAW_MEAS(var)      (var & (~IIMPC_PACK_REQUESTS_BITS_CALIB_MEAS))
+
+typedef enum
+{
+	IMMPC_MESSAGE_ID_INKNOW = 0,
+	IMMPC_MESSAGE_ID_9DOF_PACK_MAIN_,
+	IMMPC_MESSAGE_ID_9DOF_PACK_RESERVE,
+
+	IMMPC_MESSAGE_ID_ACC3DOF_CALIBMATRIX,
+	IMMPC_MESSAGE_ID_GYR3DOF_CALIBMATRIX,
+	IMMPC_MESSAGE_ID_MAG3DOF_CALIBMATRIX,
+
+	IMMPC_MESSAGE_ID_3DOF_PACK_MAIN,
+
+} immpc_message_id_e;
+
+typedef enum
+{
+	IMMPC_MESSAGE_PACK_UNKNOW = 0u,
+	IMMPC_MESSAGE_PACK_9dof_main_raw_pack_s,
+} immpc_message_struct_e;
+
+typedef struct
+{
+	uint16_t 	startFrame;
+	uint8_t 	messageID;
+	uint8_t 	packRequests;
+	uint16_t 	sensorsStatus;
+}
+#if defined (__GNUC__)
+__attribute__((__packed__))
+#else
+#error "Please, define compiler"
+#endif
+immpc_head_s;
+
+typedef struct
+{
+	immpc_head_s headMessage_s;
+	int16_t acc_a[3u];
+	int16_t gyr_a[3u];
+	int16_t mag_a[3u];
+
+	int16_t accTemp_a[3u];
+	int16_t gyrTemp_a[3u];
+	int16_t magSelfTest_a[3u];
+
+	uint16_t crc;
+}
+#if defined (__GNUC__)
+__attribute__((__packed__))
+#else
+#error "Please, define compiler"
+#endif
+immpc_9dof_main_raw_pack_s;
+
 /*#### |End  | <-- Секция - "Определение констант" ###########################*/
 
 
@@ -49,7 +217,7 @@
 /*#### |Begin| --> Секция - "Include - подмодули" ############################*/
 /*#### |End  | <-- Секция - "Include - подмодули" ############################*/
 
-#endif	/* LIB_A_IIMPC_IIM_PROTOCOL_COMMUNICATE_ */
+#endif  /* LIB_A_IIMPC_IIM_PROTOCOL_COMMUNICATE_ */
 
 /*############################################################################*/
 /*################################ END OF FILE ###############################*/
